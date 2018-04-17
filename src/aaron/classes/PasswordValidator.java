8218b63fdb05af4470c9dfd4b4f49f199fe76a3b
Caelum Forder <caelum119@gmail.com>
2018-04-17 10:07:27 +0000

    Temporarily commented out this PV so the program can compile without errors

--- src/aaron/classes/PasswordValidator.java
+++ src/aaron/classes/PasswordValidator.java
@@ -1,20 +1,20 @@
-package aaron.classes;
-
-
-/**
- * @author Aaron
- * Extracted code from other people's files
- */
-public class PasswordValidator {
-    static boolean validate(String challengePassword){
-        //Perhaps aaron.classes.details is wrong version?
-        String actualPassword = Details.users.get(Details.pin);
-
-        if(challengePassword.equals(actualPassword)) {
-            // correct password
-        } else {
-            // incorrect
-        }
-
-    }
-}
+//package aaron.classes;
+//
+//
+///**
+// * @author Aaron
+// * Extracted code from other people's files
+// */
+//public class PasswordValidator {
+//    static boolean validate(String challengePassword){
+//        //Perhaps aaron.classes.details is wrong version?
+////        String actualPassword = Details.users.get(Details.pin);
+////
+////        if(challengePassword.equals(actualPassword)) {
+////            // correct password
+////        } else {
+////            // incorrect
+////        }
+//
+//    }
+//}
