f5bedb88b1c18ad22364e454ff1ef690faabe952
caelum.forder <caelum119@gmail.com>
2018-04-17 12:18:40 +0100

    Added database for used bikes to Sherin's interface

--- src/sherins/classes/Bike.java
+++ src/sherins/classes/Bike.java
@@ -1,23 +1,21 @@
 package sherins.classes;
 
+import caelum.classes.BikeDatabase;
+
 import java.util.Scanner;
 
 public class Bike {
     private static String userName;
     private static String pin;
-
-
-
-
     public static void main(String[] args) {
         // login personal details
 
         Scanner in= new Scanner(System.in);
 
         System.out.println("Login in:");
-        System.out.println("Enter Your Name");
-        in.nextLine();
-        System.out.println("Enter Your PIN");
+        System.out.println("Enter Your userName");
+        String userName= in.nextLine();
+        System.out.println("Enter Your pin");
         String  pin = in.nextLine();
 
         System.out.println("you are logged in");
@@ -29,15 +27,26 @@ public class Bike {
 
             getBike();
         }
-
     }
     public static void getBike(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Choose available bikes between 1 to 15");
+        //Print out available bikes
+        for(int i = 0; i < BikeDatabase.availableBikes.size(); i++){
+            System.out.println(BikeDatabase.availableBikes.get(i));
+        }
+        //Validation code,
+        int userChoice=in.nextInt();
+        //Set chosen bike to not available
+        BikeDatabase.availableBikes.set(userChoice, false);
 
-
+//		String availableBikes = in.nextLine();
+//           if(userChoice !=1) {
+//        	   availableBikes="between 1 and 15";
+//        	   }
+//
+//
+//           }
     }
-
-
-
-
 }
 
