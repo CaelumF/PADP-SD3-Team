9a9ceca66463a7ec1191456a6f44f18f87f34141
Caelum Forder <caelum119@gmail.com>
2018-04-17 10:08:30 +0000

    Added EntryPoint to serve as the main start point of the program (The "Main main" method lol)

---
+++ src/caelum/classes/EntryPoint.java
@@ -0,0 +1,10 @@
+package caelum.classes;
+
+import stephen.classes.LoginMenu;
+
+public class EntryPoint {
+    public static void main(String[] args) {
+        LoginMenu.main(new String[]{""});
+
+    }
+}
