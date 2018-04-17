a95c75222c3450ce545a9db7148fbbe5d95bebfa
Caelum Forder <caelum119@gmail.com>
2018-04-17 10:09:04 +0000

    Added ScannerB description

--- src/caelum/classes/ScannerB.java
+++ src/caelum/classes/ScannerB.java
@@ -1,4 +1,9 @@
 package caelum.classes;
 
+/**
+ * This class will handle validation and provide an interface between questions.
+ *
+ * After the code has been merged, I will use Find-Replace to make the program use this instead of scanner
+ */
 public class ScannerB {
 }
