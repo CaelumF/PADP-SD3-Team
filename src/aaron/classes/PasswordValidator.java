59cf4718aa34edd7564532d1695057dcdfbb0a00
caelum.forder <caelum119@gmail.com>
2018-04-17 11:29:47 +0100

    Uncommented code following updated Details.java

--- src/aaron/classes/PasswordValidator.java
+++ src/aaron/classes/PasswordValidator.java
@@ -1,20 +1,21 @@
-//package aaron.classes;
-//
-//
-///**
-// * @author Aaron
-// * Extracted code from other people's files
-// */
-//public class PasswordValidator {
-//    static boolean validate(String challengePassword){
-//        //Perhaps aaron.classes.details is wrong version?
-////        String actualPassword = Details.users.get(Details.pin);
-////
-////        if(challengePassword.equals(actualPassword)) {
-////            // correct password
-////        } else {
-////            // incorrect
-////        }
-//
-//    }
-//}
+package aaron.classes;
+
+
+/**
+ * @author Aaron
+ * Extracted code from other people's files
+ */
+public class PasswordValidator {
+    static boolean validate(String email, String challengePassword){
+//        Perhaps aaron.classes.details is wrong version?
+        String actualPassword = Details.users.get(Details.users.get(email));
+
+        if(challengePassword.equals(actualPassword)) {
+            // correct password
+            return true;
+        } else {
+            // incorrect
+            return false;
+        }
+    }
+}
