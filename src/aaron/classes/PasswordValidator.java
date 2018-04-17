a9ea3e3dde5546e6ed7e77863932793d580917d7
Caelum Forder <caelum119@gmail.com>
2018-04-17 09:52:20 +0000

    Added and formatted' Aaron's Details.java modification, though it may be the wrong version

--- src/aaron/classes/PasswordValidator.java
+++ src/aaron/classes/PasswordValidator.java
@@ -1,4 +1,19 @@
 package aaron.classes;
 
+
+/**
+ * @author Aaron
+ */
 public class PasswordValidator {
+    static boolean validate(String challengePassword){
+        //Perhaps aaron.classes.details is wrong version?
+        String actualPassword = Details.users.get(Details.pin);
+
+        if(challengePassword.equals(actualPassword)) {
+            // correct password
+        } else {
+            // incorrect
+        }
+
+    }
 }
